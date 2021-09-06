97286f195c7342ab694cadc2b1a533d6b240b62a
Anum Rukhsar <anumrukhsar@Abduls-MacBook-Pro.local>
2021-09-06 17:25:01 +0500

    layout a widget

--- lib/main.dart
+++ lib/main.dart
@@ -19,100 +19,121 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DocsListingPage extends StatelessWidget{
+class DocsListingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(title:Center(child: Text('Flutter Docs Demo'))),
-     body: DocsListingScreen(),
-
-   );
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text('Flutter Docs Demo'))),
+      body: DocsListingScreen(),
+    );
   }
 }
 
-class DocsListingScreen extends StatelessWidget{
-
+class DocsListingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(20.0),
-      child: ListView(
-          children:[
+      child: ListView(children: [
         IntroWidget(),
         LayoutWidget(),
         NavigationWidget(),
         AppStateMangementWidget(),
-            
       ]),
     );
   }
 }
 
-class IntroWidget extends StatelessWidget{
+class IntroWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return ElevatedButton(onPressed: ()=>{
-     Navigator.push(context, MaterialPageRoute(builder: (context)=>WidgetsIntro(title: 'Introduction Widget')))
-   }, child: Text('Introduction Widget'));
+    return ElevatedButton(
+        onPressed: () => {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          WidgetsIntro(title: 'Introduction Widget')))
+            },
+        child: Text('Introduction Widget'));
   }
 }
 
-class LayoutWidget extends StatelessWidget{
+class LayoutWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: ()=>{}, child: Text('Layout Widget'));
+    return ElevatedButton(
+        onPressed: () => {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          LayoutAWidget(title: 'Layout a widget')))
+            },
+        child: Text('Layout Widget'));
   }
 }
-class NavigationWidget extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: ()=>{
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>NavigationRouteWidget(title: 'Navigation Widget')))
 
-    }, child: Text('Navigation Widget'));
-  }
-}
-class AppStateMangementWidget extends StatelessWidget{
+class NavigationWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: ()=>{}, child: Text('AppStateMangement Widget'));
+    return ElevatedButton(
+        onPressed: () => {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          NavigationRouteWidget(title: 'Navigation Widget')))
+            },
+        child: Text('Navigation Widget'));
+  }
+}
+
+class AppStateMangementWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () => {}, child: Text('AppStateMangement Widget'));
   }
 }
 
 class WidgetsIntro extends StatelessWidget {
   final String title;
+
   const WidgetsIntro({required this.title});
+
   @override
   Widget build(BuildContext context) {
     // A Center widget that shows Hello World
-    return Scaffold(appBar: AppBar(title: Text(this.title),),
-    body:
-      Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Hello World'),
-          GestureDetector(
-            onTap: () {
-              print("I was Tapped");
-            },
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              margin: EdgeInsets.symmetric(horizontal: 10.0),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5.0),
-                  color: Colors.deepPurpleAccent),
-              child: Text('Gesture Detector',
-                  style:
-                  TextStyle(fontSize: 12.0, fontStyle: FontStyle.normal)),
-            ),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(this.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Hello World'),
+              GestureDetector(
+                onTap: () {
+                  print("I was Tapped");
+                },
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  margin: EdgeInsets.symmetric(horizontal: 10.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.0),
+                      color: Colors.deepPurpleAccent),
+                  child: Text('Gesture Detector',
+                      style: TextStyle(
+                          fontSize: 12.0, fontStyle: FontStyle.normal)),
+                ),
+              ),
+              Counter(),
+              ShoppingList(items: ['Chips', 'Ice Cream']),
+            ],
           ),
-          Counter(),
-          ShoppingList(items: ['Chips', 'Ice Cream']),
-
-        ],
-      ),
-    ));
+        ));
   }
 }
 
@@ -133,10 +154,10 @@ class _CounterState extends State<Counter> {
         Container(
             margin: EdgeInsets.all(10.0),
             child:
-            ElevatedButton(onPressed: increment, child: Text('Increase'))),
+                ElevatedButton(onPressed: increment, child: Text('Increase'))),
         Container(
             child:
-            ElevatedButton(onPressed: decrement, child: Text('Decrease')))
+                ElevatedButton(onPressed: decrement, child: Text('Decrease')))
       ],
     );
   }
@@ -168,50 +189,59 @@ class _ShoppingListState extends State<ShoppingList> {
 
   @override
   Widget build(BuildContext context) {
-    return
-      ListTile(
-        onTap: () =>
-        {
-          setState(() =>
-          {
-            inCart = !inCart
-          })
-        },
-        leading: CircleAvatar(child: Text(widget.items[0][0]),backgroundColor: inCart ? Colors.blue : Colors.grey,),
-        title: Text(widget.items[0], style: TextStyle(
+    return ListTile(
+      onTap: () => {
+        setState(() => {inCart = !inCart})
+      },
+      leading: CircleAvatar(
+        child: Text(widget.items[0][0]),
+        backgroundColor: inCart ? Colors.blue : Colors.grey,
+      ),
+      title: Text(
+        widget.items[0],
+        style: TextStyle(
             color: inCart ? Colors.blue : Colors.grey,
-            decoration: inCart ? TextDecoration.lineThrough : null),),
-      );
-     ListView(
+            decoration: inCart ? TextDecoration.lineThrough : null),
+      ),
+    );
+    ListView(
       children: widget.items
           .map((e) => ListTile(
                 onTap: () => {
-                  setState(()=>{
-                    inCart=!inCart
-                  })
+                  setState(() => {inCart = !inCart})
                 },
                 leading: CircleAvatar(child: Text(e[0])),
-                title: Text(e,style: TextStyle(color: inCart?Colors.blue:Colors.grey,decoration: inCart ?TextDecoration.lineThrough:null),),
+                title: Text(
+                  e,
+                  style: TextStyle(
+                      color: inCart ? Colors.blue : Colors.grey,
+                      decoration: inCart ? TextDecoration.lineThrough : null),
+                ),
               ))
           .toList(),
-
     );
   }
 }
 
 class NavigationRouteWidget extends StatelessWidget {
   final title;
+
   const NavigationRouteWidget({required this.title});
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: Text('Navigation Widget'),),
-    body:
-      Center(child: GestureDetector(onTap: () {
-      goToNewPage(context);
-    },
-      child: Container(
-          padding: EdgeInsets.all(10.0), child: Text("Go To New page")),)));
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+            child: GestureDetector(
+          onTap: () {
+            goToNewPage(context);
+          },
+          child: Container(
+              padding: EdgeInsets.all(10.0), child: Text("Go To New page")),
+        )));
   }
 }
 
@@ -221,19 +251,22 @@ class NewPage extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(title: Text('New Page'),),
+            appBar: AppBar(
+              title: Text('New Page'),
+            ),
             body: Center(
-                child: GestureDetector(onTap: () => {goBack(context)},
-                  child: Container(
-                    padding: EdgeInsets.all(10.0), child: Text("Go Back"),),
-                ))));
+                child: GestureDetector(
+              onTap: () => {goBack(context)},
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text("Go Back"),
+              ),
+            ))));
   }
-
 }
 
 void goToNewPage(BuildContext context) {
-  Navigator.push(
-      context,  MaterialPageRoute(builder: (context) => NewPage()));
+  Navigator.push(context, MaterialPageRoute(builder: (context) => NewPage()));
 }
 
 void goBack(BuildContext context) {
@@ -246,45 +279,39 @@ class NavigationWidgetUsingRoutes extends StatelessWidget {
     return MaterialApp(
       routes: {
         '/': (context) => Home(),
-        '/details' : (context) => DetailPage(),
+        '/details': (context) => DetailPage(),
       },
     );
   }
 }
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          body:
-          ElevatedButton(
-            onPressed: ()=>{Navigator.pushNamed(context, '/details/2')},
-            child: Text("Detail"),
-          ),
-
-        ));
+      body: ElevatedButton(
+        onPressed: () => {Navigator.pushNamed(context, '/details/2')},
+        child: Text("Detail"),
+      ),
+    ));
   }
 }
 
-class DetailPage extends StatelessWidget{
+class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          body:
-          ElevatedButton(
-            onPressed: ()=>{Navigator.pop(context)},
-            child: Text("Back Home"),
-          ),
-
-        ));
-
-
+      body: ElevatedButton(
+        onPressed: () => {Navigator.pop(context)},
+        child: Text("Back Home"),
+      ),
+    ));
   }
 }
 
-class NavWidgetUsingRoutesWithParams extends StatelessWidget{
+class NavWidgetUsingRoutesWithParams extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -305,26 +332,123 @@ class NavWidgetUsingRoutesWithParams extends StatelessWidget{
         // return MaterialPageRoute(builder: (context) => UnknownScreen());
       },
     );
-
   }
-
-
 }
-class DetailScreen extends StatelessWidget{
+
+class DetailScreen extends StatelessWidget {
   final id;
+
   DetailScreen({this.id});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Container(
+      body: Container(
         child: Center(
           child: Text('Detail $id'),
         ),
-      ) ,
+      ),
     );
   }
 }
 
+class LayoutAWidget extends StatelessWidget {
+  final title;
 
+  const LayoutAWidget({required this.title});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Row(children: [
+                Expanded(
+                    child: Image.network('https://picsum.photos/250?image=9')),
+                Expanded(
+                    flex: 2,
+                    child: Image.network('https://picsum.photos/250?image=9')),
+                Expanded(
+                    child: Image.network('https://picsum.photos/250?image=9')),
+              ]),
+              Column(
+                children: [
+                  Text(
+                    'Strawberry Paravola',
+                    style:
+                        TextStyle(fontStyle: FontStyle.italic, fontSize: 20.0),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.star),
+                          Icon(Icons.star),
+                          Icon(Icons.star),
+                          Icon(
+                            Icons.star,
+                            color: Colors.deepPurple,
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.deepPurple,
+                          ),
+                        ],
+                      ),
+                      Text('153 Reviews'),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Icon(Icons.timelapse),
+                          Text('TimeLapse'),
+                          Text('14 hrs'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.food_bank),
+                          Text('Feeds'),
+                          Text('4-6'),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Icon(Icons.timelapse_sharp),
+                          Text('Preperation'),
+                          Text('25 min'),
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+           Stack(
+             alignment: Alignment(0.6,0.6),
+             children: [
+               CircleAvatar(
+                 backgroundImage: NetworkImage('https://picsum.photos/250?image=9'),
+                 radius: 100,
+               ),
+               Container(
+                 color: Colors.black45,
+                 child: Text('Mira',style: TextStyle(color: Colors.white),),
 
+               )
+             ],
+           ),
+            ],
+          ),
+        ));
+  }
+}
