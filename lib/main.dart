e729af1c36979f07807b15f07db020e91d37930a
Anum Rukhsar <anumrukhsar@Abduls-MacBook-Pro.local>
2021-09-06 14:24:44 +0500

    Flutter Docs Demo

---
+++ lib/main.dart
@@ -0,0 +1,330 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Docs Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: DocsListingPage(),
+    );
+  }
+}
+
+class DocsListingPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(title:Center(child: Text('Flutter Docs Demo'))),
+     body: DocsListingScreen(),
+
+   );
+  }
+}
+
+class DocsListingScreen extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      child: ListView(
+          children:[
+        IntroWidget(),
+        LayoutWidget(),
+        NavigationWidget(),
+        AppStateMangementWidget(),
+            
+      ]),
+    );
+  }
+}
+
+class IntroWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return ElevatedButton(onPressed: ()=>{
+     Navigator.push(context, MaterialPageRoute(builder: (context)=>WidgetsIntro(title: 'Introduction Widget')))
+   }, child: Text('Introduction Widget'));
+  }
+}
+
+class LayoutWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: ()=>{}, child: Text('Layout Widget'));
+  }
+}
+class NavigationWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: ()=>{
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>NavigationRouteWidget(title: 'Navigation Widget')))
+
+    }, child: Text('Navigation Widget'));
+  }
+}
+class AppStateMangementWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: ()=>{}, child: Text('AppStateMangement Widget'));
+  }
+}
+
+class WidgetsIntro extends StatelessWidget {
+  final String title;
+  const WidgetsIntro({required this.title});
+  @override
+  Widget build(BuildContext context) {
+    // A Center widget that shows Hello World
+    return Scaffold(appBar: AppBar(title: Text(this.title),),
+    body:
+      Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Hello World'),
+          GestureDetector(
+            onTap: () {
+              print("I was Tapped");
+            },
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              margin: EdgeInsets.symmetric(horizontal: 10.0),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  color: Colors.deepPurpleAccent),
+              child: Text('Gesture Detector',
+                  style:
+                  TextStyle(fontSize: 12.0, fontStyle: FontStyle.normal)),
+            ),
+          ),
+          Counter(),
+          ShoppingList(items: ['Chips', 'Ice Cream']),
+
+        ],
+      ),
+    ));
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Count:$counter'),
+        Container(
+            margin: EdgeInsets.all(10.0),
+            child:
+            ElevatedButton(onPressed: increment, child: Text('Increase'))),
+        Container(
+            child:
+            ElevatedButton(onPressed: decrement, child: Text('Decrease')))
+      ],
+    );
+  }
+
+  void increment() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      if (counter > 0) counter--;
+    });
+  }
+}
+
+class ShoppingList extends StatefulWidget {
+  final List<String> items;
+
+  const ShoppingList({required this.items, Key? key}) : super(key: key);
+
+  @override
+  _ShoppingListState createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  bool inCart = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      ListTile(
+        onTap: () =>
+        {
+          setState(() =>
+          {
+            inCart = !inCart
+          })
+        },
+        leading: CircleAvatar(child: Text(widget.items[0][0]),backgroundColor: inCart ? Colors.blue : Colors.grey,),
+        title: Text(widget.items[0], style: TextStyle(
+            color: inCart ? Colors.blue : Colors.grey,
+            decoration: inCart ? TextDecoration.lineThrough : null),),
+      );
+     ListView(
+      children: widget.items
+          .map((e) => ListTile(
+                onTap: () => {
+                  setState(()=>{
+                    inCart=!inCart
+                  })
+                },
+                leading: CircleAvatar(child: Text(e[0])),
+                title: Text(e,style: TextStyle(color: inCart?Colors.blue:Colors.grey,decoration: inCart ?TextDecoration.lineThrough:null),),
+              ))
+          .toList(),
+
+    );
+  }
+}
+
+class NavigationRouteWidget extends StatelessWidget {
+  final title;
+  const NavigationRouteWidget({required this.title});
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(title: Text('Navigation Widget'),),
+    body:
+      Center(child: GestureDetector(onTap: () {
+      goToNewPage(context);
+    },
+      child: Container(
+          padding: EdgeInsets.all(10.0), child: Text("Go To New page")),)));
+  }
+}
+
+class NewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(title: Text('New Page'),),
+            body: Center(
+                child: GestureDetector(onTap: () => {goBack(context)},
+                  child: Container(
+                    padding: EdgeInsets.all(10.0), child: Text("Go Back"),),
+                ))));
+  }
+
+}
+
+void goToNewPage(BuildContext context) {
+  Navigator.push(
+      context,  MaterialPageRoute(builder: (context) => NewPage()));
+}
+
+void goBack(BuildContext context) {
+  Navigator.pop(context);
+}
+
+class NavigationWidgetUsingRoutes extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/': (context) => Home(),
+        '/details' : (context) => DetailPage(),
+      },
+    );
+  }
+}
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          body:
+          ElevatedButton(
+            onPressed: ()=>{Navigator.pushNamed(context, '/details/2')},
+            child: Text("Detail"),
+          ),
+
+        ));
+  }
+}
+
+class DetailPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          body:
+          ElevatedButton(
+            onPressed: ()=>{Navigator.pop(context)},
+            child: Text("Back Home"),
+          ),
+
+        ));
+
+
+  }
+}
+
+class NavWidgetUsingRoutesWithParams extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: (settings) {
+        // Handle '/'
+        if (settings.name == '/') {
+          return MaterialPageRoute(builder: (context) => Home());
+        }
+
+        // Handle '/details/:id'
+        var uri = Uri.parse(settings.name.toString());
+        if (uri.pathSegments.length == 2 &&
+            uri.pathSegments.first == 'details') {
+          var id = uri.pathSegments[1];
+          return MaterialPageRoute(builder: (context) => DetailScreen(id: id));
+        }
+
+        // return MaterialPageRoute(builder: (context) => UnknownScreen());
+      },
+    );
+
+  }
+
+
+}
+class DetailScreen extends StatelessWidget{
+  final id;
+  DetailScreen({this.id});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Container(
+        child: Center(
+          child: Text('Detail $id'),
+        ),
+      ) ,
+    );
+  }
+}
+
+
+
+
